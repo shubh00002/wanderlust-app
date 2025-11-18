8c198ae0a9f30c43e83c3ec70f1125af5af5c779
shubh sharma <141862373+shubh00002@users.noreply.github.com>
2025-11-18 21:01:56 +0530

    Fix import path for wrapAsync utility

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const wrapAsync = require("../views/utils/wrapAsync");
+const wrapAsync = require("../utils/wrapAsync");
 const User = require("../models/user.js");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
